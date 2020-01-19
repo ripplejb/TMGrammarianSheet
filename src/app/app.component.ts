4301d246f6bd0ff5c6669e6d28c1ea24e836c585
Ripal Barot <ripplejb@hotmail.com>
2020-01-19 15:20:02 -0600

    Removed Media matcher. Now it is working on mobile.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {Observable, Subscription} from 'rxjs';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
 import {SheetService} from './sheet-service/sheet.service';
 import {Speaker} from './models/Speaker';
 
@@ -11,22 +10,16 @@ import {Speaker} from './models/Speaker';
 })
 export class AppComponent  implements OnDestroy, OnInit {
   title = 'Grammarian Sheet';
-  mobileQuery: MediaQueryList;
 
   speakers: Array<Speaker>;
   speakersSubscription: Subscription;
 
-  private readonly mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private sheetService: SheetService) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addEventListener('change', this.mobileQueryListener);
+  constructor(private sheetService: SheetService) {
     this.speakersSubscription = this.sheetService.speakers$.subscribe(speakers => this.speakers = speakers);
   }
 
   ngOnDestroy(): void {
-      this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
       this.speakersSubscription.unsubscribe();
   }
 
